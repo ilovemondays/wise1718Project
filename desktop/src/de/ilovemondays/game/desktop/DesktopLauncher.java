2b548a569ab834c19c92c8b2e8819feb3d2efb81
matthiasdietrich <mat.dietrich1985@gmail.com>
2017-09-30 17:56:26 +0200

    init libgdx with box2d + lighting

---
+++ desktop/src/de/ilovemondays/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.ilovemondays.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.ilovemondays.game.Semester3Project;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Semester3Project(), config);
+	}
+}
